250f73574c4e237a590edae9fe1491cdb9ba06f7
julianbautista <julian.bautista@port.ac.uk>
2022-02-22 00:30:55 +0100

    log version

--- make_movies.py
+++ make_movies.py
@@ -46,15 +46,20 @@ def xi_animation(data, rmin=0, rmax=300, i_range=None, interval=30):
     conformal_time = data['conformal_time']
     sound_horizon = data['sound_horizon']
     redshifts = data['redshifts']
-    names = ['Baryons', 'Cold Dark Matter', 'Photons', 'Neutrinos']
-    linestyles = ['-', '-', '--', ':']
-    colors = ['C0', 'k', 'C3', 'C1']
+    names = ['Baryons', 'Cold Dark Matter', 'Photons', 'Neutrinos']#, 'Total Matter']
+    n_species = len(names)
+
+    c_ls = {'Baryons': ['C0','-'], 
+                  'Cold Dark Matter': ['purple', '-'],
+                  'Photons': ['C3', '--'],
+                  'Neutrinos': ['C1', ':'],
+                  'Total Matter': ['k', '-']}
 
     fig, ax = plt.subplots()
     fig.subplots_adjust(left=0.2)
     lines = []
-    for j in range(4):
-        line, = ax.plot([], [], color=colors[j], ls=linestyles[j], lw=2, label=names[j])
+    for name in names:
+        line, = ax.plot([], [], color=c_ls[name][0], ls=c_ls[name][1], lw=2, label=name)
         lines.append(line)
     line = ax.axvline(0, color='k', ls='--', alpha=1, lw=1, label='Horizon')
     lines.append(line)
@@ -75,7 +80,7 @@ def xi_animation(data, rmin=0, rmax=300, i_range=None, interval=30):
     def update(frame):
 
         ydatas = []
-        for j in range(4):
+        for j in range(n_species):
             line = lines[j]
             ydata = xi[:, frame, j] * r**2
             line.set_data(r, ydata)
@@ -86,9 +91,9 @@ def xi_animation(data, rmin=0, rmax=300, i_range=None, interval=30):
         ymax = np.max(ydatas)
         ymin_ = ymin - 0.05*(ymax-ymin)
         ymax_ = ymax + 0.05*(ymax-ymin)
-        line = lines[4]
+        line = lines[n_species]
         line.set_xdata(conformal_time[frame])
-        line = lines[5]
+        line = lines[n_species+1]
         rs = sound_horizon[frame]
         if rs <= data['rdrag']:
             line.set_xdata(rs)
@@ -109,7 +114,8 @@ def xi_animation(data, rmin=0, rmax=300, i_range=None, interval=30):
                         init_func=init, blit=False)
     return ani
 
-def pk_animation(data, kmin=1e-4, kmax=10, scale_k=2, i_range=None, interval=30):
+def pk_animation(data, kmin=1e-4, kmax=10, scale_k=2, y_logscale=False, 
+    i_range=None, interval=30):
 
 
     k = data['k']
@@ -121,16 +127,20 @@ def pk_animation(data, kmin=1e-4, kmax=10, scale_k=2, i_range=None, interval=30)
     conformal_time = data['conformal_time']
     sound_horizon = data['sound_horizon']
     redshifts = data['redshifts']
-    names = ['Baryons', 'Cold Dark Matter', 'Photons', 'Neutrinos']
-    linestyles = ['-', '-', '--', ':']
-    colors = ['C0', 'k', 'C3', 'C1']
+    names = ['Baryons', 'Cold Dark Matter', 'Photons', 'Neutrinos']#, 'Total Matter']
+    n_species = len(names)
 
+    c_ls = {'Baryons': ['C0','-'], 
+                  'Cold Dark Matter': ['purple', '-'],
+                  'Photons': ['C3', '--'],
+                  'Neutrinos': ['C1', ':'],
+                  'Total Matter': ['k', '-']}
 
     fig, ax = plt.subplots()
     fig.subplots_adjust(left=0.2)
     lines = []
-    for j in range(4):
-        line, = ax.plot([], [], color=colors[j], ls=linestyles[j], lw=2, label=names[j])
+    for name in names:
+        line, = ax.plot([], [], color=c_ls[name][0], ls=c_ls[name][1], lw=2, label=name)
         lines.append(line)
     line = ax.axvline(1e-4, color='k', ls='--', alpha=1, lw=1, label='Horizon')
     lines.append(line)
@@ -150,21 +160,22 @@ def pk_animation(data, kmin=1e-4, kmax=10, scale_k=2, i_range=None, interval=30)
             ax.set_ylabel(rf'$k^{{{scale_k}}} P(k)$', fontsize=12)
         ax.set_xlabel(r'$k$ [Mpc$^{-1}$]', fontsize=12)
         ax.set_xscale('log')
-        #ax.set_yscale('log')
+        if y_logscale:
+            ax.set_yscale('log')
         return lines,
 
     def update(frame):
 
         ydatas = []
-        for j in range(4):
+        for j in range(n_species):
             line = lines[j]
             ydata = pk[:, frame, j] * k**scale_k
             line.set_data(k, ydata)
             ydatas.append(ydata)
 
-        line = lines[4]
+        line = lines[n_species]
         line.set_xdata(1/conformal_time[frame])
-        line = lines[5]
+        line = lines[n_species+1]
         rs = sound_horizon[frame]
         if rs <= data['rdrag']:
             line.set_xdata(1/rs)
@@ -173,7 +184,10 @@ def pk_animation(data, kmin=1e-4, kmax=10, scale_k=2, i_range=None, interval=30)
 
         ymin = np.min(ydatas)
         ymax = np.max(ydatas)
-        ax.set_ylim(ymin - 0.05*(ymax-ymin), ymax + 0.05*(ymax-ymin))
+        if y_logscale:
+            ax.set_ylim(ymax*1e-4, ymax*10)
+        else:
+            ax.set_ylim(ymin - 0.05*(ymax-ymin), ymax + 0.05*(ymax-ymin))
         ax.set_title(f'z = {redshifts[frame]:.1f}', fontsize=12)
         ax.legend(loc='upper left')
         ax.figure.canvas.draw()
@@ -192,11 +206,16 @@ def save_anim(anim, fname, fps=60, **kwargs):
 data = load('camb_outputs.pkl')
 
 print('Making pk video....')
-anim = pk_animation(data, scale_k=0, i_range = np.arange(500, 1000), interval=100)
-save_anim(anim, 'pk_movie_v2.mp4', fps=30, dpi=200)
+anim = pk_animation(data, scale_k=0, y_logscale=False, i_range = np.arange(500, 1000), interval=100)
+save_anim(anim, 'pk_movie_ylinear.mp4', fps=30, dpi=200)
+#plt.show()
+
+print('Making pk video....')
+anim = pk_animation(data, scale_k=0, y_logscale=True, i_range = np.arange(500, 1000), interval=100)
+save_anim(anim, 'pk_movie_ylog.mp4', fps=30, dpi=200)
 #plt.show()
 
 print('Making xi video....')
 anim_xi = xi_animation(data, i_range = np.arange(500, 1000), interval=100)
-save_anim(anim_xi, 'xi_movie_v2.mp4', fps=30, dpi=200)
+save_anim(anim_xi, 'xi_movie.mp4', fps=30, dpi=200)
 #plt.show()
--- write_camb_transfer_functions.py
+++ write_camb_transfer_functions.py
@@ -15,10 +15,7 @@ def load(filename):
     output = pickle.load(open(filename, 'rb'))
     return output
 
-#Set up a new set of parameters for CAMB
 pars = camb.CAMBparams(scalar_initial_condition='initial_adiabatic')
-
-#This function sets up CosmoMC-like settings, with one massive neutrino and helium set using BBN consistency
 pars_cosmo = {'ombh2': 0.0224, 
               'omch2': 0.120, 
               'mnu': 0.00, 
@@ -26,22 +23,20 @@ pars_cosmo = {'ombh2': 0.0224,
               'cosmomc_theta':1.0411/100,
               'omk':0,
               }
-
 pars_init = {'As': np.exp(3.043)*1e-10, 
              'ns': 0.9652}
 
+accuracy = 3
 pars.set_cosmology(H0=None, **pars_cosmo)
 pars.InitPower.set_params(r=0, **pars_init)
-#pars.set_for_lmax(2500, lens_potential_accuracy=0)
-pars.set_accuracy(AccuracyBoost=3, lAccuracyBoost=3)
-#pars.set_matter_power(redshifts=[0., 0.8], kmax=10.0)
+pars.set_accuracy(AccuracyBoost=accuracy, lAccuracyBoost=accuracy)
+
+pars.set_matter_power(redshifts=[0.], kmax=10.0)
+results = camb.get_results(pars)
+kh, _, pk0 = results.get_matter_power_spectrum(minkh=1e-3, maxkh=1, npoints = 200)
 
 data = camb.get_transfer_functions(pars)
 
-#pars.set_matter_power(redshifts=[0.], kmax=10.0)
-#results = camb.get_results(pars)
-
-#h, z, pk = results.get_matter_power_spectrum(minkh=1e-3, maxkh=1, npoints = 200)
 
 scale_factor  = 10**np.linspace(-10, 0, 1000)
 redshifts = 1/scale_factor - 1 
@@ -54,44 +49,55 @@ nk = 1024
 ks = 10**np.linspace(-4, 2, nk)
 
 species = ['delta_baryon','delta_cdm', 'delta_photon', 'delta_neutrino',
-            'delta_tot', 'Weyl']
+           'delta_tot', 'Weyl']
+names = ['Baryons', 'Cold Dark Matter', 'Photons', 'Neutrinos', 'Total Matter', 'Weyl Potential']
+n_species = len(species)
+
 
 time0 = time.time()
 print('Computing evolution...')
-evolution = data.get_redshift_evolution(ks, redshifts, species, lAccuracyBoost=5)
+evolution = data.get_redshift_evolution(ks, redshifts, species, lAccuracyBoost=accuracy)
 time1 = time.time()
 print(f'Done in {(time1-time0)/60:.3f} minutes.')
 
 eta = data.conformal_time(redshifts)
 sound_horizon = data.sound_horizon(redshifts)
 
-
+h = pars.h
 pk_init = 4/9*pars_init['As']*(ks/0.05)**(pars_init['ns']-1) * (2*np.pi**2/ks**3)
 
-pk_final = pk_init[:, None, None] * evolution**2
+pk_final = pk_init[:, None, None] * evolution**2 /h**3
 
-pk_final[:, :, 0] *= (2/3)**2 
+#-- Adiabatic initial conditions
+
+#-- baryons
+pk_final[:, :, 0] *= (2/3)**2
+#-- cdm
 pk_final[:, :, 1] *= (2/3)**2
+#-- photons
 pk_final[:, :, 2] *= 1/4
+#-- neutrinos
 pk_final[:, :, 3] *= 1/4
-pk_final[:, :, 4] *= 1/4
+#-- Total matter (not correct)
+pk_final[:, :, 4] *= 2/4
 
 
 time2 = time.time()
-print('Computing hankel transforms...')
+print('Computing Hankel transforms to obtain correlation functions...')
 xi_final = np.zeros_like(evolution) 
 for i in range(nsteps):   
-    for j in range(4):        
+    for j in range(n_species):        
         r, xi = hankl.P2xi(ks, pk_final[:, i, j], 0, n=0, lowring=False, ext=0, range=None, return_ext=False)
         xi_final[:, i, j] = xi.real*1
 time3 = time.time()
 print(f'Done in {(time3-time2)/60:.3f} minutes.')
 
-output = {'pars_cosmo': pars_cosmo,
+output = {  'pars_cosmo': pars_cosmo,
             'pars_init': pars_init, 
             'scale_factor': scale_factor,
             'redshifts': redshifts,
             'species': species,
+            'names': names,
             'conformal_time': eta,
             'sound_horizon': sound_horizon,  
             'k': ks, 
